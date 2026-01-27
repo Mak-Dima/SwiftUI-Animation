71a7c827412f660d88863fdb40d2c72f66a9b10f
maksiutenkodmytro <maksiutenko.dmytro88@gmail.com>
2026-01-27 16:58:43 -0600

    Initial Commit

---
+++ SwiftUI-Animation/SwiftUI_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AnimationApp.swift
+//  SwiftUI-Animation
+//
+//  Created by Dmytro Maksiutenko on 2026-01-27.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI-AnimationTests/SwiftUI_AnimationTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AnimationTests.swift
+//  SwiftUI-AnimationTests
+//
+//  Created by Dmytro Maksiutenko on 2026-01-27.
+//
+
+import Testing
+@testable import SwiftUI_Animation
+
+struct SwiftUI_AnimationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
